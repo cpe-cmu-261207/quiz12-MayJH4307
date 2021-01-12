fd92295d84e5d0841befd50f6fde1994eea6a767
MayJh4307 <norit.hunhan@hotmail.com>
2021-01-12 13:57:10 +0700

    p1

--- app_template.js
+++ app_template.js
@@ -1,1 +1,23 @@
 //your code here!
+
+let numIN = document.querySelector('#input-number');
+let idSubIn = document.querySelector('#input-number');
+let n = document.getElementById('input-number');
+var nu = idSubIn.value;
+let btnsum = document.querySelector('button');
+var myid = 620610818;
+var sum = 0;
+let num = numIN.value;
+let div = document.querySelector('div');
+
+
+btnsum.addEventListener('click' , () => {
+    myid += idSubIn.value;
+    let template = `
+        <p>${idSubIn.value}</P>
+        <p>${myid}</p>        
+    `;
+    div.innerHTML += template;
+})
+
+
